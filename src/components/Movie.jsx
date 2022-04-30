b70231752d57ca847a3e03cca3c260e16eea500b
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-04-29 21:35:17 -0500

    added movies to MovieList

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,7 +4,6 @@ const Movie = (props) => {
   return (
     <div className="card">
     <div className="top">
-      <p>{props.id}</p>
       <h1>{props.title}</h1>
       <h2 className="name">{props.name}</h2>
       <img src={props.img} alt={`${props.name} Movie`} />
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,14 +3,22 @@ const MovieList = [
         id: 1,
         title: 'Godfather',
         director: 'Francis Ford Coppola',
-        imgURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-5A46hyz-axE%2FXm4TFV55ORI%2FAAAAAAAB2U0%2FcpdS-dhuQT4Qy9WPgjcxRl-gyXoW9HY8gCLcBGAsYHQ%2Fs1600%2F6195-the-godfather-1280x800-movie-wallpaper.png&f=1&nofb=1",
+        imgURL: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY209_CR3,0,140,209_AL_.jpg",
         releaseDate: 1972
     },
     {
         id: 2,
-        title: 'Big Trouble in Little China',
-        director: 'John Carpenter',
-        releaseDate: 1986,
+        title: "Schindler's List",
+        director: 'Steven Spielberg',
+        imgURL: 'https://m.media-amazon.com/images/M/MV5BNDE4OTMxMTctNmRhYy00NWE2LTg3YzItYTk3M2UwOTU5Njg4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX140_CR0,0,140,209_AL_.jpg',
+        releaseDate: 1993,
+    }, 
+    {
+        id: 3,
+        title: "The Silence of the Lambs",
+        director: ' Jonathan Demme',
+        imgURL: 'https://m.media-amazon.com/images/M/MV5BNjNhZTk0ZmEtNjJhMi00YzFlLWE1MmEtYzM1M2ZmMGMwMTU4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY209_CR1,0,140,209_AL_.jpg',
+        releaseDate: 1991,
     }
 
 ]
