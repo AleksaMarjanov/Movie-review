48f94021f7d938e8b19ca435e61e391dc9999e8b
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-05-04 20:37:22 -0500

    added poster images

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,29 +1,28 @@
 const MovieList = [
-    {
-        id: 1,
-        title: 'Godfather',
-        director: 'Francis Ford Coppola',
-        imgURL: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY209_CR3,0,140,209_AL_.jpg",
-        releaseDate: 1972
-    },
-    {
-        id: 2,
-        title: "Schindler's List",
-        director: 'Steven Spielberg',
-        imgURL: 'https://m.media-amazon.com/images/M/MV5BNDE4OTMxMTctNmRhYy00NWE2LTg3YzItYTk3M2UwOTU5Njg4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX140_CR0,0,140,209_AL_.jpg',
-        releaseDate: 1993,
-    }, 
-    {
-        id: 3,
-        title: "The Silence of the Lambs",
-        director: 'Jonathan Demme',
-        imgURL: 'https://m.media-amazon.com/images/M/MV5BNjNhZTk0ZmEtNjJhMi00YzFlLWE1MmEtYzM1M2ZmMGMwMTU4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY209_CR1,0,140,209_AL_.jpg',
-        releaseDate: 1991,
-    }
+  {
+    id: 1,
+    title: "Godfather",
+    director: "Francis Ford Coppola",
+    imgURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.reelgood.com%2Fcontent%2Fmovie%2F86307584-c177-4967-bf7e-8c5dc1afaa97%2Fposter-780.jpg&f=1&nofb=1",
+    releaseDate: 1972,
+  },
+  {
+    id: 2,
+    title: "Schindler's List",
+    director: "Steven Spielberg",
+    imgURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.goldenglobes.com%2Fsites%2Fdefault%2Ffiles%2Farticles%2Fcover_images%2F1994-schindlers_list.jpg&f=1&nofb=1",
+    releaseDate: 1993,
+  },
+  {
+    id: 3,
+    title: "The Silence of the Lambs",
+    director: "Jonathan Demme",
+    imgURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F8f%2F3c%2F18%2F8f3c1801560fbe3f953615ef3d465db0--fun-recipes-jodie-foster.jpg&f=1&nofb=1",
+    releaseDate: 1991,
+  },
+];
 
-]
-    
-   
- 
-
-export default MovieList
+export default MovieList;
--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -4,7 +4,7 @@ import Review from "./Review";
 const ReviewList = ({ username, review, rating }) => {
   return (
     <>
-      <div className="overflow-auto" id="reviewBox">
+      <div className="mt-0" id="reviewBox">
         <Review rating={rating} />
         <p>Your rating is: {rating} of 5</p>
         <p>{username}</p>
